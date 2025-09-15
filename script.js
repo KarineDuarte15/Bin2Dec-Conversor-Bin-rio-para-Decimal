0a0fa034f4b05ea8ae4bcc2af560c8f812907978
KarinDuarte15 <karinegaebrasil@gmail.com>
2025-09-15 13:17:02 -0300

    Adicionar referências aos elementos HTML no script

---
+++ script.js
@@ -0,0 +1,4 @@
+ // 1. Pegar referências aos elementos HTML que vamos usar
+const binaryInput = document.getElementById('binaryInput');
+const decimalOutput = document.getElementById('decimalOutput');
+const errorMessage = document.getElementById('errorMessage');  
